c918818f78d2e87a76ef6e7bf3e61b2e03ba81a4
Jayesh Khandelwal <jayeshkwal2209@gmail.com>
2026-01-05 13:35:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function scrollDown() {
-    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
-}
-
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Message submitted successfully!");
-    this.reset();
-});
